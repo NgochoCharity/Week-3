89587ac82b769a73500ea90f3dcab7142188a75c
NgochoCharity <ngochocharity@gmail.com>
2022-05-15 13:42:23 +0300

    adds an if else loop to display if answer is correct or wrong

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,3 +37,30 @@
 
           quizContainer.innerHTML = output.join('');
     }
+    
+    function showResults(){
+  
+    
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+    
+        let numCorrect = 0;
+    
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+    
+          const answerContainer = answerContainers[questionNumber];
+          const selector = `input[name=question${questionNumber}]:checked`;
+          const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+           // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+            
+            numCorrect++;
+    
+            answerContainers[questionNumber].style.color = 'purple';
+          }
+          
+          else{
+            
+            answerContainers[questionNumber].style.color = 'red';
+          }
+        });
